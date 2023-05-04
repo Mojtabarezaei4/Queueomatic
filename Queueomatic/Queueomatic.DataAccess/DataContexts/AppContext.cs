82d370ed4f7d52a32239deeb5cadcf351aaeac3d
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-05 01:11:06 +0200

    Add corresponding DbSet for each datamodel

--- Queueomatic/Queueomatic.DataAccess/DataContexts/AppContext.cs
+++ Queueomatic/Queueomatic.DataAccess/DataContexts/AppContext.cs
@@ -1,9 +1,12 @@
 ﻿using Microsoft.EntityFrameworkCore;
+using Queueomatic.DataAccess.Models;
 
 namespace Queueomatic.DataAccess.DataContexts;
 
 public class AppContext : DbContext
 {
+    public DbSet<User> Users { get; set; }
+    public DbSet<Room> Rooms { get; set; }
     public AppContext(DbContextOptions<AppContext> options) : base(options)
     {
 
