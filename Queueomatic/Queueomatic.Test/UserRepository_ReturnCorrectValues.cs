098bc680338daea28160b9daf5211512edf5e245
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-05 16:28:07 +0200

    Add nuget package

---
+++ Queueomatic/Queueomatic.Test/UserRepository_ReturnCorrectValues.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Queueomatic.DataAccess.DataContexts;
+
+namespace Queueomatic.Test;
+
+public class UserRepository_ReturnCorrectValues
+{
+    [Fact]
+    public async Task GetUser_ReturnUserOrNull()
+    {
+        //Arrange
+        var options = new DbContextOptionsBuilder<ApplicationContext>()
+            .UseInMemoryDatabase(databaseName: "StoreDatabase")
+            .Options;
+        //Act
+
+        //Assert
+    }
+}
