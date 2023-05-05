249ab5a05d4bf06ccf0b3710200639725494d229
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-05 15:39:42 +0200

    Create base interface

---
+++ Queueomatic/Queueomatic.DataAccess/Repositories/Interfaces/IRepository.cs
@@ -0,0 +1,11 @@
+﻿namespace Queueomatic.DataAccess.Repositories.Interfaces;
+
+public interface IRepository<T> where T : class
+{
+    Task<T?> GetAsync(Guid id);
+    Task<IEnumerable<T>> GetAllAsync();
+    Task AddAsync(T entity);
+    Task UpdateAsync(T entity);
+    Task DeleteAsync(Guid id);
+    
+}
