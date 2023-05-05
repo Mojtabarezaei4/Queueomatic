a3bb8675383813baa136a211f511f8ae043f12d0
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-05 15:44:31 +0200

    Create User repository

---
+++ Queueomatic/Queueomatic.DataAccess/Repositories/UserRepository.cs
@@ -0,0 +1,40 @@
+﻿using Queueomatic.DataAccess.DataContexts;
+using Queueomatic.DataAccess.Models;
+using Queueomatic.DataAccess.Repositories.Interfaces;
+
+namespace Queueomatic.DataAccess.Repositories;
+
+public class UserRepository : IUserRepository
+{
+    private readonly ApplicationContext _context;
+
+    public UserRepository(ApplicationContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<User?> GetAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<User>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task AddAsync(User entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateAsync(User entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
