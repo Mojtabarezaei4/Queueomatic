7e0faa961c01d2fbdc73a1b4bad1223705ce5e9d
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-05 15:55:13 +0200

    Add base Room repository

---
+++ Queueomatic/Queueomatic.DataAccess/Repositories/RoomRepository.cs
@@ -0,0 +1,40 @@
+﻿using Queueomatic.DataAccess.DataContexts;
+using Queueomatic.DataAccess.Models;
+using Queueomatic.DataAccess.Repositories.Interfaces;
+
+namespace Queueomatic.DataAccess.Repositories;
+
+public class RoomRepository : IRoomRepository
+{
+    private readonly ApplicationContext _context;
+
+    public RoomRepository(ApplicationContext context)
+    {
+        _context = context;
+    }
+
+    public async Task<Room?> GetAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task<IEnumerable<Room>> GetAllAsync()
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task AddAsync(Room entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task UpdateAsync(Room entity)
+    {
+        throw new NotImplementedException();
+    }
+
+    public async Task DeleteAsync(Guid id)
+    {
+        throw new NotImplementedException();
+    }
+}
