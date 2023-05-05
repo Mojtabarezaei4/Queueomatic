5c324e45ff257211b12eaaafd756bffb40a767bb
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-06 00:55:36 +0200

    Add room unit test

---
+++ Queueomatic/Queueomatic.Test/RoomRepository_ReturnCorrectValues.cs
@@ -0,0 +1,33 @@
+﻿using FakeItEasy;
+using Microsoft.EntityFrameworkCore;
+using Queueomatic.DataAccess.DataContexts;
+using Queueomatic.DataAccess.Models;
+using Queueomatic.DataAccess.Repositories.Interfaces;
+using Queueomatic.DataAccess.UnitOfWork;
+
+namespace Queueomatic.Test;
+
+public class RoomRepository_ReturnCorrectValues
+{
+    [Fact]
+    public async Task GetRoom_ReturnRoomOrNull()
+    {
+        //Arrange
+        var options = new DbContextOptionsBuilder<ApplicationContext>()
+            .UseInMemoryDatabase(databaseName: "QueueomaticTestDatabase")
+            .Options;
+
+        var room = A.Fake<Room>();
+        var userRepository = A.Fake<IUserRepository>();
+        var roomRepository = A.Fake<IRoomRepository>();
+        A.CallTo(() => roomRepository.GetAsync(room.Id)).Returns(room);
+
+        //Act   
+        await using var context = new ApplicationContext(options);
+        var sut = new UnitOfWork(context, userRepository, roomRepository);
+        var result = await sut.RoomRepository.GetAsync(room.Id);
+
+        //Assert
+        Assert.Equivalent(room, result);
+    }
+}
