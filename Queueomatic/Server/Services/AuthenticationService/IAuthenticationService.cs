c2449648c00c596001b307a5e5deb8e8d673ba57
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-09 21:58:34 +0200

    Add CreatePasswordHash method

--- Queueomatic/Server/Services/AuthenticationService/IAuthenticationService.cs
+++ Queueomatic/Server/Services/AuthenticationService/IAuthenticationService.cs
@@ -6,5 +6,4 @@ public interface IAuthenticationService
 {
     public Task Register(User user);
     public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
-    public Task<bool> UserCredentialsValid(string  username, string password);
 }
