8d181886c11f9fecfa4f9995641280e0fde2ef04
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-09 21:58:46 +0200

    Add password validation method

--- Queueomatic/Server/Services/AuthenticationService/IAuthenticationService.cs
+++ Queueomatic/Server/Services/AuthenticationService/IAuthenticationService.cs
@@ -6,4 +6,5 @@ public interface IAuthenticationService
 {
     public Task Register(User user);
     public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
+    public Task<bool> AreUserCredentialsValid(string username, string password);
 }
