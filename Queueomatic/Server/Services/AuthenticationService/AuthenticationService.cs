1609dfb466c456d09c8aacfd558dd038b50dc4d6
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-09 21:58:22 +0200

    Add register method

--- Queueomatic/Server/Services/AuthenticationService/AuthenticationService.cs
+++ Queueomatic/Server/Services/AuthenticationService/AuthenticationService.cs
@@ -1,6 +1,7 @@
-﻿namespace Queueomatic.Server.Services.AuthenticationService;
+﻿using Queueomatic.DataAccess.Models;
+
+namespace Queueomatic.Server.Services.AuthenticationService;
 
 public class AuthenticationService : IAuthenticationService
 {
-    
 }
--- Queueomatic/Server/Services/AuthenticationService/IAuthenticationService.cs
+++ Queueomatic/Server/Services/AuthenticationService/IAuthenticationService.cs
@@ -1,6 +1,10 @@
-﻿namespace Queueomatic.Server.Services.AuthenticationService;
+﻿using Queueomatic.DataAccess.Models;
+
+namespace Queueomatic.Server.Services.AuthenticationService;
 
 public interface IAuthenticationService
 {
-    
+    public Task Register(User user);
+    public void CreatePasswordHash(string password, out byte[] passwordHash, out byte[] passwordSalt);
+    public Task<bool> UserCredentialsValid(string  username, string password);
 }
