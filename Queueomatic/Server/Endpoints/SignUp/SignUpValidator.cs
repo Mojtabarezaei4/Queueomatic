04a0c56248fa6c34df6c15eebe3a3a7c815d058a
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-14 17:35:28 +0200

    Add password validation

--- Queueomatic/Server/Endpoints/SignUp/SignUpValidator.cs
+++ Queueomatic/Server/Endpoints/SignUp/SignUpValidator.cs
@@ -1,6 +1,20 @@
-﻿namespace Queueomatic.Server.Endpoints.SignUp;
+﻿using FastEndpoints;
+using FluentValidation;
 
-public class SignUpValidator
+namespace Queueomatic.Server.Endpoints.SignUp;
+
+public class SignUpValidator : Validator<SignUpRequest>
 {
-    
+    public SignUpValidator()
+    {
+        RuleFor(x => x.Signup.Password)
+            .NotEmpty().WithMessage("A password is required!")
+            .MinimumLength(10).WithMessage("Please provide a longer password")
+            .Matches(@"[A-Z]+").WithMessage("Your password must contain at least one uppercase letter.")
+            .Matches(@"[a-z]+").WithMessage("Your password must contain at least one lowercase letter.")
+            .Matches(@"[0-9]+").WithMessage("Your password must contain at least one number.")
+            .Matches(@"[\!\?\*\.]+").WithMessage("Your password must contain at least one (!? *.).");
+
+
+    }
 }
