a01f4aafbe0a58dc968ce44c24976b48be433297
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-15 11:18:22 +0200

    Update response values

--- Queueomatic/Server/Endpoints/SignUp/SignUpEndpoint.cs
+++ Queueomatic/Server/Endpoints/SignUp/SignUpEndpoint.cs
@@ -1,32 +1,34 @@
-﻿using System.Text;
-using FastEndpoints;
-using Queueomatic.DataAccess.UnitOfWork;
+﻿using FastEndpoints;
 using Queueomatic.Server.Services.AuthenticationService;
 
 namespace Queueomatic.Server.Endpoints.SignUp;
 
-public class SignUpEndpoint : Endpoint<SignUpRequest, SignUpResponse>
+public class SignUpEndpoint : Endpoint<SignUpRequest>
 {
+    private readonly IAuthenticationService _authenticationService;
+
+    public SignUpEndpoint(IAuthenticationService authenticationService)
+    {
+        _authenticationService = authenticationService;
+    }
+
     public override void Configure()
     {
         Verbs(Http.POST);
         Routes("/signup");
         AllowAnonymous();
     }
-    public IAuthenticationService AuthenticationService { get; set; }
-
+    
     public override async Task HandleAsync(SignUpRequest req, CancellationToken ct)
     {
         req.Signup.NickName ??= string.Empty;
 
-        var response = new SignUpResponse();
-
-        if (!await AuthenticationService.Register(req.Signup))
+        if (!await _authenticationService.Register(req.Signup))
         {
-            await SendAsync(response, 400, cancellation: ct);
+            await SendAsync("User already exists.", 400);
             return; 
         }
 
-        await SendCreatedAtAsync<SignUpEndpoint>("", response, cancellation: ct);
+        await SendCreatedAtAsync<SignUpEndpoint>("api/login", "Successfully registered!");
     }
 }
