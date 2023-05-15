dc5cc74227a009b812eef6714e5275cfb5c2fece
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-15 13:20:25 +0200

    Add email validation

--- Queueomatic/Server/Endpoints/Login/LoginValidator.cs
+++ Queueomatic/Server/Endpoints/Login/LoginValidator.cs
@@ -1,4 +1,5 @@
 ﻿using FastEndpoints;
+using FluentValidation;
 
 namespace Queueomatic.Server.Endpoints.Login;
 
@@ -6,6 +7,11 @@ public class LoginValidator : Validator<LoginRequest>
 {
     public LoginValidator()
     {
-        
+        RuleFor(x => x.Login.Email)
+            .NotNull().WithMessage("Email can not be empty!")
+            .Matches(
+                "^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$")
+            .WithMessage("Not a valid email address.");
+
     }
 }
