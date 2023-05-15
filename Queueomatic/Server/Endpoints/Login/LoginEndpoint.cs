4785611886f1837a643063b2444446edb63307a2
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-15 13:15:19 +0200

    Clean up unnecessary code

--- Queueomatic/Server/Endpoints/Login/LoginEndpoint.cs
+++ Queueomatic/Server/Endpoints/Login/LoginEndpoint.cs
@@ -2,7 +2,7 @@
 
 namespace Queueomatic.Server.Endpoints.Login;
 
-public class LoginEndpoint: Endpoint<LoginRequest, LoginResponse>
+public class LoginEndpoint: Endpoint<LoginRequest>
 {
     public override void Configure()
     {
@@ -13,20 +13,6 @@ public class LoginEndpoint: Endpoint<LoginRequest, LoginResponse>
 
     public override async Task HandleAsync(LoginRequest req, CancellationToken ct)
     {
-        try
-        {
-            var response = new LoginResponse();
-            await SendAsync(response, cancellation: ct);
-        }
-        catch (NullReferenceException nullException)
-        {
-            Logger.LogInformation($"The request can not be null.\nMessage: {nullException.Message}");
-            await SendAsync(response:null, 400, ct);
-        }
-        catch (TaskCanceledException exception)
-            when(exception.CancellationToken == ct)
-        {
-            Logger.LogInformation($"Task {nameof(LoginEndpoint)} was cancelled.");
-        }
+       
     }
 }
