91d85ffde5041276ce46aabe1fa76d99194f1088
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-16 15:27:21 +0200

    Add dependencies

--- Queueomatic/Server/Endpoints/User/GetByEmail/GetUserByEmailEndpoint.cs
+++ Queueomatic/Server/Endpoints/User/GetByEmail/GetUserByEmailEndpoint.cs
@@ -1,32 +1,33 @@
 ﻿using FastEndpoints;
+using Queueomatic.DataAccess.UnitOfWork;
 using Queueomatic.Shared.DTOs;
 
 namespace Queueomatic.Server.Endpoints.User.GetByEmail;
 
 public class GetUserByEmailEndpoint: Endpoint<GetUserByEmailRequest, GetUserByEmailResponse>
 {
+    private readonly IUnitOfWork _unitOfWork;
+
+    public GetUserByEmailEndpoint(IUnitOfWork unitOfWork)
+    {
+        _unitOfWork = unitOfWork;
+    }
+
     public override void Configure()
     {
-        Verbs(Http.GET);
-        Routes("/users/{email}");
-        AllowAnonymous();
+        Get("/users/{email}");
+        AllowAnonymous();       
     }
 
     public override async Task HandleAsync(GetUserByEmailRequest req, CancellationToken ct)
     {
-        try
-        {
+        
             var response = new GetUserByEmailResponse(new UserDto());
             if (response is null) await SendAsync(new GetUserByEmailResponse(null), 404, cancellation: ct);
             else
             {
                 await SendAsync(response, cancellation: ct);
             }
-        }
-        catch (TaskCanceledException exception)
-            when(exception.CancellationToken == ct)
-        {
-            Logger.LogInformation($"Task {nameof(GetUserByEmailEndpoint)} was cancelled.");
-        }
+        
     }
 }
