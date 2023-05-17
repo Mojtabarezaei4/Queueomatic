0a7c868f1c68aee4add1a58b6fc10f4f45605e3d
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-17 13:32:36 +0200

    Add interface methods

--- Queueomatic/Server/Services/RoomService/IRoomService.cs
+++ Queueomatic/Server/Services/RoomService/IRoomService.cs
@@ -1,8 +1,14 @@
-﻿using Queueomatic.Shared.DTOs;
+﻿using Queueomatic.DataAccess.Models;
+using Queueomatic.Shared.DTOs;
 
 namespace Queueomatic.Server.Services.RoomService;
 
 public interface IRoomService
 {
     public Task<bool> CreateRoomAsync(RoomDto room, string userEmail);
+
+    public RoomDto ToEntity(Room room);
+    public IEnumerable<RoomDto> ToEntity(IEnumerable<Room> room);
+    public RoomDto FromEntity(Room room);
+    public IEnumerable<RoomDto> FromEntity(IEnumerable<Room> room); 
 }
