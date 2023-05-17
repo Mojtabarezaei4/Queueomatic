449736c4f5b437af825204990947728f0d567ad8
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-17 09:16:54 +0200

    Add more dependencies

--- Queueomatic/Server/Services/HashIdService/HashIdService.cs
+++ Queueomatic/Server/Services/HashIdService/HashIdService.cs
@@ -1,6 +1,25 @@
-﻿namespace Queueomatic.Server.Services.HashIdService;
+﻿using HashidsNet;
 
-public class HashIdService
+namespace Queueomatic.Server.Services.HashIdService;
+
+public class HashIdService : IHashIdService
 {
+	private readonly Hashids _hashids;
+	private readonly IConfiguration _configuration;
+
+	public HashIdService(Hashids hashids, IConfiguration configuration)
+	{
+		_hashids = hashids;
+		_configuration = configuration;
+	}
 	
+	public string Encode(int id)
+	{
+		
+	}
+
+	public int Decode(string hash)
+	{
+		throw new NotImplementedException();
+	}
 }
