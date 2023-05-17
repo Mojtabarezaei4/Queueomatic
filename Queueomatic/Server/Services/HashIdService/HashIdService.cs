44d69e1813bffa0adccf6463deb2d68d9320167c
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-17 09:35:21 +0200

    Add salt

--- Queueomatic/Server/Services/HashIdService/HashIdService.cs
+++ Queueomatic/Server/Services/HashIdService/HashIdService.cs
@@ -1,21 +1,22 @@
 ﻿using HashidsNet;
+using Microsoft.Extensions.Configuration;
 
 namespace Queueomatic.Server.Services.HashIdService;
 
 public class HashIdService : IHashIdService
 {
 	private readonly Hashids _hashids;
-	private readonly IConfiguration _configuration;
+	private readonly string _salt;
 
 	public HashIdService(Hashids hashids, IConfiguration configuration)
 	{
 		_hashids = hashids;
-		_configuration = configuration;
+		_salt = configuration.GetSection("HashIdKey").GetSection("Default").Value;
 	}
-	
+
 	public string Encode(int id)
 	{
-		
+
 	}
 
 	public int Decode(string hash)
