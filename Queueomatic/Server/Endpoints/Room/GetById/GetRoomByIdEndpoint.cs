785269f2d0b1a83e867b72e989afde9267cb6d36
mojtabarezaei4 <mojtabarezaei2001@gmail.com>
2023-05-18 14:16:10 +0200

    dummy

--- Queueomatic/Server/Endpoints/Room/GetById/GetRoomByIdEndpoint.cs
+++ Queueomatic/Server/Endpoints/Room/GetById/GetRoomByIdEndpoint.cs
@@ -1,40 +1,31 @@
 ﻿using FastEndpoints;
+using Queueomatic.DataAccess.UnitOfWork;
+using Queueomatic.Server.Services.RoomService;
 using Queueomatic.Shared.DTOs;
 
 namespace Queueomatic.Server.Endpoints.Room.GetById;
 
 public class GetRoomByIdEndpoint : Endpoint<GetRoomByIdRequest, GetRoomByIdResponse>
 {
+    private readonly IUnitOfWork _unitOfWork;
+    private readonly IRoomService _roomService;
+
+    public GetRoomByIdEndpoint(IUnitOfWork unitOfWork, IRoomService roomService)
+    {
+        _unitOfWork = unitOfWork;
+        _roomService = roomService;
+    }
+
     public override void Configure()
     {
-        Verbs(Http.GET);
-        Routes("/rooms/{id}");
+        Get("/rooms/{id}");
         AllowAnonymous();
     }
 
     public override async Task HandleAsync(GetRoomByIdRequest req, CancellationToken ct)
     {
-        try
-        {
-            var response = new GetRoomByIdResponse(new RoomDto()
-            {
-                Name = "Test",
-                Owner = null,
-                Participators = new List<ParticipantDto>(),
-                CreatedAt = DateTime.UtcNow,
-                ExpireAt = DateTime.UtcNow,
-                HashIds = req.Id
-            });
-            if (response is null) await SendAsync(new GetRoomByIdResponse(null), 404, cancellation: ct);
-            else
-            {
-                await SendAsync(response, cancellation: ct);
-            }
-        }
-        catch (TaskCanceledException exception)
-            when (exception.CancellationToken == ct)
-        {
-            Logger.LogInformation($"Task {nameof(GetRoomByIdEndpoint)} has cancelled.");
-        }
+        // var response = _unitOfWork.RoomRepository.GetAsync(_roomService.ToEntity(req.Id)));
+        
+        await SendAsync(null, cancellation: ct);
     }
 }
---
+++ Queueomatic/Server/Endpoints/Room/GetById/GetRoomByIdValidator.cs
@@ -0,0 +1,15 @@
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace Queueomatic.Server.Endpoints.Room.GetById;
+
+public class GetRoomByIdValidator : Validator<GetRoomByIdRequest>
+{
+    public GetRoomByIdValidator()
+    {
+        RuleFor(x => x.Id)
+            .Length(6)
+            .NotNull()
+            .NotEmpty();
+    }
+}
