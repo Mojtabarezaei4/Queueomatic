ea68b2c1917c1759da2e1519cf46e12cdb0b9a45
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-19 16:52:44 +0200

    Update request

--- Queueomatic/Server/Endpoints/Room/GetAll/GetAllRoomsEndpoint.cs
+++ Queueomatic/Server/Endpoints/Room/GetAll/GetAllRoomsEndpoint.cs
@@ -24,6 +24,6 @@ public class GetAllRoomsEndpoint : Endpoint<GetAllRoomRequest, GetAllRoomsRespon
 
     public override async Task HandleAsync(GetAllRoomRequest req, CancellationToken ct)
     {
-        var rooms = await _unitOfWork.RoomRepository.GetAllAsync();
+        var rooms = await _unitOfWork.RoomRepository.GetAllAsync(req.RoomId, req.RoomName);
     }
 }
