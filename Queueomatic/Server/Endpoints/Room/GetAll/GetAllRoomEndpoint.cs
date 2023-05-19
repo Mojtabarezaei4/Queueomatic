730005554a19d2e1427e21615538e9604a9ebe39
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-19 16:35:40 +0200

    Remove unnecessary code

--- Queueomatic/Server/Endpoints/Room/GetAll/GetAllRoomEndpoint.cs
+++ Queueomatic/Server/Endpoints/Room/GetAll/GetAllRoomEndpoint.cs
@@ -7,22 +7,13 @@ public class GetAllRoomEndpoint : Endpoint<GetAllRoomRequest, GetAllRoomResponse
 {
     public override void Configure()
     {
-        Verbs(Http.GET);
-        Routes("/rooms");
-        AllowAnonymous();
+        Get("/rooms");
+        Roles("Administrator");
+        
     }
 
     public override async Task HandleAsync(GetAllRoomRequest req, CancellationToken ct)
     {
-        try
-        {
-            var response = new GetAllRoomResponse(new List<RoomDto>());
-            await SendAsync(response, cancellation: ct);
-        }
-        catch (TaskCanceledException exception)
-            when (exception.CancellationToken == ct)
-        {
-            Logger.LogInformation($"Task {nameof(GetAllRoomEndpoint)} has cancelled");
-        }
+       
     }
 }
