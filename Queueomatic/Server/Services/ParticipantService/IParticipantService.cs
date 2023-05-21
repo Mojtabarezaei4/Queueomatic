094a5b06752cb425dedc607d1b18f139af22adea
Mojtabarezaei4 <mojtabarezaei2001@gmail.com>
2023-05-21 23:26:08 +0200

    Feat: Add a service for manage participators

---
+++ Queueomatic/Server/Services/ParticipantService/IParticipantService.cs
@@ -0,0 +1,13 @@
+﻿using Queueomatic.DataAccess.Models;
+using Queueomatic.Shared.DTOs;
+
+namespace Queueomatic.Server.Services.ParticipantService;
+
+public interface IParticipantService
+{
+    Task<bool> CreateOneAsync(ParticipantDto participantDto);
+
+    ParticipantDto FromModel(Participant participant);
+
+    Participant ToModel(ParticipantDto participantDto);
+}
