3ecae628c14f17801f9849a7d6d54d42da0a132c
Mojtabarezaei4 <mojtabarezaei2001@gmail.com>
2023-05-25 19:50:25 +0200

    Test: More test cases for creating new participants

--- Queueomatic/Queueomatic.Test/ParticipantService_AddParticipant.cs
+++ Queueomatic/Queueomatic.Test/ParticipantService_AddParticipant.cs
@@ -9,17 +9,27 @@ namespace Queueomatic.Test;
 
 public class ParticipantService_AddParticipant
 {
-    [Fact]
-    public async Task AddParticipant_ReturnParticipantDto()
+    [Theory]
+    [InlineData(1)]
+    [InlineData(25)]
+    [InlineData(623)]
+    [InlineData(6185)]
+    [InlineData(1231)]
+    [InlineData(1615)]
+    [InlineData(3241)]
+    [InlineData(324)]
+    [InlineData(74)]
+    public async Task AddParticipant_ReturnParticipantDto(int roomId)
     {
         // Arrange
         var unitOfWork = A.Fake<IUnitOfWork>();
         var participantDto = A.Fake<ParticipantDto>();
         var hashidsService = A.Fake<IHashIdService>();
+        var encodedRoomId = hashidsService.Encode(roomId);
         var sut = new ParticipantService(unitOfWork, hashidsService);
         participantDto.StatusDate = DateTime.UtcNow;
         // Act
-        var result = await sut.CreateOneAsync(participantDto, "satezx");
+        var result = await sut.CreateOneAsync(participantDto, encodedRoomId);
 
         // Assert
         Assert.IsType<ParticipantDto>(result);
