030f72a743caceecdab4030234dd45716e617dfb
mojtabarezaei4 <mojtabarezaei2001@gmail.com>
2023-05-30 15:31:41 +0200

    Update: Add data anotation in signupDto for validation in frontend

--- Queueomatic/Shared/DTOs/SignupDto.cs
+++ Queueomatic/Shared/DTOs/SignupDto.cs
@@ -1,9 +1,26 @@
-﻿namespace Queueomatic.Shared.DTOs;
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Queueomatic.Shared.DTOs;
 
 public class SignupDto
 {
+    [Required(ErrorMessage = "Email is required")]
+    [DataType(DataType.Text)]
     public string Email { get; set; }
+    
+    [Required(ErrorMessage = "Nickname is required")]
+    [DataType(DataType.EmailAddress)]
     public string? NickName { get; set; }
+    
+    [Required(ErrorMessage = "Password is required")]
+    [StringLength(255, ErrorMessage = "Password must be 10 characters at minimum", MinimumLength = 10)]
+    [RegularExpression(@"[A-Z]+[a-z]+[0-9]+[\!\?\*\.]+", ErrorMessage = "Your password must contain at least one uppercase letter, one lowercase letter, one number, one (!? *.)")]
+    [DataType(DataType.Password)]
     public string Password { get; set; }
+    
+    [Required(ErrorMessage = "Confirm Password is required")]
+    [StringLength(255, ErrorMessage = "Confirm Password must be 10 characters at minimum", MinimumLength = 10)]
+    [DataType(DataType.Password)]
+    [Compare("Password")]
     public string ConfirmPassword { get; set; }
 }
