547dde7ff44cc035134cf35328032b03a512066d
FabianAronsson <fabianaronsson2@gmail.com>
2023-05-31 00:54:56 +0200

    Abstract code into separate file

--- Queueomatic/Client/Pages/Room.razor.cs
+++ Queueomatic/Client/Pages/Room.razor.cs
@@ -1,6 +1,27 @@
-﻿namespace Queueomatic.Client.Pages;
+﻿using Microsoft.AspNetCore.Components;
+using Queueomatic.Shared.DTOs;
 
-public class Room_razor
+namespace Queueomatic.Client.Pages;
+
+public partial class Room : ComponentBase
 {
-    
+    [Parameter]
+    public string RoomId { get; set; }
+
+    [Parameter]
+    public string ParticipantName { get; set; }
+
+    public string RoomName { get; set; } = "TestRoom";
+
+    public List<string> IdlingParticipants = new() { "Erik", "Jan" };
+    public List<string> WaitingParticipants = new() { "John", "Eliza" };
+    public List<string> ActiveParticipants = new() { "Keller", "Rebecca" };
+
+    private bool CanMove(string user) => user.Equals(ParticipantName); //should be updated with participant Id
+
+
+    private void UpdateUser(string user, StatusDto status)
+    {
+        //Update SignalR Clients
+    }
 }
