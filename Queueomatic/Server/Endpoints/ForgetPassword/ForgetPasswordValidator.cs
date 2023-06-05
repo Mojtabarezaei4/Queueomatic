554d5ed4ace9680591ae0c04f85cbf31015dec4b
coldmagpie <coldmagpie@gmail.com>
2023-06-05 09:54:51 +0200

    add forget password request validator

---
+++ Queueomatic/Server/Endpoints/ForgetPassword/ForgetPasswordValidator.cs
@@ -0,0 +1,17 @@
+﻿using FastEndpoints;
+using FluentValidation;
+
+namespace Queueomatic.Server.Endpoints.ForgetPassword
+{
+    public class ForgetPasswordValidator : Validator<ForgetPasswordRequest>
+    {
+        public ForgetPasswordValidator()
+        {
+            RuleFor(x => x.Email)
+                .NotNull().WithMessage("Email can not be empty!")
+                .Matches(
+                    "^[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?$")
+                .WithMessage("Not a valid email address!");
+        }
+    }
+}
