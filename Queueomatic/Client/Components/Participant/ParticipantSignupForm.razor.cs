617325c2f754eb30989011d7bd57d27e8a4b4617
Mojtabarezaei4 <mojtabarezaei2001@gmail.com>
2023-06-17 20:43:48 +0200

    Feat: Add a modal for participant to enter a room

---
+++ Queueomatic/Client/Components/Participant/ParticipantSignupForm.razor.cs
@@ -0,0 +1,39 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using BlazorBootstrapToasts;
+using Microsoft.AspNetCore.Components;
+using Queueomatic.Shared.DTOs;
+
+namespace Queueomatic.Client.Components.Participant;
+
+public partial class ParticipantSignupForm: ComponentBase
+{
+    private ParticipantDto _participantDto = new ();
+    
+    [Parameter] 
+    public string? RoomName { get; set; }
+    [Parameter] 
+    public string? RoomId { get; set; }
+    private Toast Toast { get; set; }
+    private async Task JoinTheRoom()
+    {
+        var addParticipantRequest = new AddParticipantRequest(_participantDto);
+        
+        var response = await HttpClient.PostAsJsonAsync($"api/rooms/{RoomId}/newParticipant", addParticipantRequest);
+
+        if (response.StatusCode == HttpStatusCode.BadRequest)
+        {
+            Toast.Show("warning", "Something went wrong", 5000);
+        }
+        
+        if (response.IsSuccessStatusCode)
+        {
+            var result = await response.Content.ReadFromJsonAsync<PostResult>();
+            await SessionStorageService.SetItemAsync("authToken", result);
+            NavigationManager.NavigateTo($"rooms/{RoomId}/{_participantDto.NickName}");
+        }
+    }
+}
+
+record AddParticipantRequest(ParticipantDto Participant);
+record PostResult(string token, string participantName);
---
+++ Queueomatic/Client/Pages/Index.razor.cs
@@ -0,0 +1,47 @@
+﻿using System.Net;
+using System.Net.Http.Json;
+using BlazorBootstrapToasts;
+using Blazored.Modal;
+using Blazored.Modal.Services;
+using Microsoft.AspNetCore.Components;
+using Queueomatic.Client.Components.Participant;
+using Queueomatic.Shared.DTOs;
+
+namespace Queueomatic.Client.Pages;
+
+public partial class Index: ComponentBase
+{
+    private bool _isClicked = false;
+    private string _buttonContent = "Join";
+    [CascadingParameter] 
+    IModalService SignupModal { get; set; }
+    private string _roomId = String.Empty;
+    private Toast Toast { get; set; }
+    private async Task CheckRoomIsValid()
+    {
+        _isClicked = true;
+        _buttonContent = "Processing...";
+        var response = await HttpClient.GetAsync($"api/rooms/{_roomId}");
+
+        if (response.StatusCode == HttpStatusCode.NotFound)
+        {
+            Toast.Show("warning", "No room with provided id was found!", 5000);
+            _isClicked = false;
+            _buttonContent = "Join";
+        }
+
+        if (response.StatusCode == HttpStatusCode.OK)
+        {
+            var room = await response.Content.ReadFromJsonAsync<RoomResponse>();
+            
+            var parameters = new ModalParameters()
+                .Add(nameof(ParticipantSignupForm.RoomName),room!.Room.Name)
+                .Add(nameof(ParticipantSignupForm.RoomId), room!.Room.HashIds);
+            SignupModal.Show<ParticipantSignupForm>(null, parameters);
+            
+            _isClicked = false;
+            _buttonContent = "Join";
+        }
+    }
+}
+record RoomResponse(RoomDto Room);
