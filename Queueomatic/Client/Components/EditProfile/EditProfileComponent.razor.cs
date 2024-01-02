afcebbdc52e6a54046bf43c7c19cf47bbb6bf4e6
FabianAronsson <fabianaronsson2@gmail.com>
2024-01-02 19:31:53 +0100

    Create event handler

--- Queueomatic/Client/Components/EditProfile/EditProfileComponent.razor.cs
+++ Queueomatic/Client/Components/EditProfile/EditProfileComponent.razor.cs
@@ -1,19 +1,24 @@
 ﻿using BlazorBootstrapToasts;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using Queueomatic.Shared.DTOs;
 
 namespace Queueomatic.Client.Components.EditProfile;
 
-public partial class EditProfileComponent: ComponentBase
+public partial class EditProfileComponent : ComponentBase
 {
 
     [Parameter] 
     public UserDto User { get; set; }
+
+    [Parameter]
+    public Action CloseModalEvent { get; set; }
     private Toast Toast { get; set; }
 
     // TODO: Update function
     private void Update()
     {
         Toast.Show("warning", "This functionality is not working right now.", 5000);
+        CloseModalEvent?.Invoke();
     }
 }
--- Queueomatic/Client/Components/Profile/ProfileComponent.razor.cs
+++ Queueomatic/Client/Components/Profile/ProfileComponent.razor.cs
@@ -3,6 +3,7 @@ using System.Net.Http.Json;
 using Blazored.Modal;
 using Blazored.Modal.Services;
 using Microsoft.AspNetCore.Components;
+using Microsoft.AspNetCore.Components.Web;
 using Queueomatic.Client.Components.EditProfile;
 using Queueomatic.Client.Components.Participant;
 using Queueomatic.Shared.DTOs;
@@ -13,9 +14,12 @@ public partial class ProfileComponent : ComponentBase
 {
     private UserDto _user = new();
     private UserDto _updatedUserInfo = new();
+    private IModalReference? _modalReference;
 
     [CascadingParameter] 
     IModalService EditProfileModal { get; set; }
+
+   
 
     protected override async Task OnInitializedAsync()
     {
@@ -42,9 +46,13 @@ public partial class ProfileComponent : ComponentBase
     private void OpenModal()
     {
         var parameters = new ModalParameters()
-            .Add(nameof(EditProfileComponent.User), _user);
-        EditProfileModal.Show<EditProfileComponent>(null, parameters);
+            .Add(nameof(EditProfileComponent.User), _user)
+            .Add(nameof(EditProfileComponent.CloseModalEvent), new Action(CloseModal));
+        _modalReference = EditProfileModal.Show<EditProfileComponent>(null, parameters);
     }
+
+    private void CloseModal() =>
+        _modalReference?.Close();
 }
 
 record ResultFromGet(UserDto user); 
