aae407f1b3b3ae05b26015392b6056883eaff726
FabianAronsson <fabianaronsson2@gmail.com>
2024-01-02 01:45:49 +0100

    Create interface

---
+++ Queueomatic/Server/Endpoints/Hubs/Room/IRoomClient.cs
@@ -0,0 +1,11 @@
+﻿using Queueomatic.DataAccess.Models;
+using Queueomatic.Shared.DTOs;
+
+namespace Queueomatic.Server.Endpoints.Hubs.Room;
+
+public interface IRoomClient
+{
+    public Task MoveParticipant(ParticipantRoomDto participantToBeUpdated, Status status);
+    public Task ClearRoom(ParticipantRoomDto participant);
+    public Task KickParticipant();
+}
