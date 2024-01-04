49d7d3e574d81d51aeb1e894f43581701a59eba7
Mojje R <mojtabarezaei2001@gmail.com>
2024-01-04 11:53:28 +0100

    Refactor: Refactor the UpdateLocalStorage method from big junk of ifs to methods

--- Queueomatic/Client/Services/ManageVisitedRooms/ManageVisitedRooms.cs
+++ Queueomatic/Client/Services/ManageVisitedRooms/ManageVisitedRooms.cs
@@ -14,40 +14,52 @@ public class ManageVisitedRooms : IManageVisitedRooms
     public async Task UpdateLocalStorage(string visitingRoom)
     {
         const string localStorageName = "visitedRooms";
+        const int maxVisitedRooms = 3;
 
         var visitedRooms = await _localStorageService.GetItemAsync<SortedDictionary<DateTime, string>>(localStorageName);
 
-        if (visitedRooms is null ||
-            !visitedRooms.Any())
+        if (IsNullOrEmpty(visitedRooms))
         {
             visitedRooms = new SortedDictionary<DateTime, string>
             {
-                { DateTime.UtcNow , visitingRoom }
+                {
+                    DateTime.UtcNow, visitingRoom
+                }
             };
 
             await _localStorageService.SetItemAsync(localStorageName, visitedRooms);
-
             return;
         }
 
-        if (visitedRooms.ContainsValue(visitingRoom))
-        {
-            visitedRooms.Remove(
-                visitedRooms
-                    .FirstOrDefault(pair => string.Equals(pair.Value, visitingRoom))
-                    .Key
-            );
-        }
+        RemoveVisitedRoom(visitedRooms, visitingRoom);
 
         visitedRooms.Add(DateTime.UtcNow, visitingRoom);
 
-        var top = visitedRooms.FirstOrDefault();
-
-        if (top.Value is not null && visitedRooms.Count > 3)
-        {
-            visitedRooms.Remove(top.Key);
-        }
+        RemoveOldestVisitedRoom(visitedRooms, maxVisitedRooms);
 
         await _localStorageService.SetItemAsync(localStorageName, visitedRooms);
+    }
+
+    private bool IsNullOrEmpty(SortedDictionary<DateTime, string>? dictionary)
+    {
+        return dictionary is null || !dictionary.Any();
+    }
+
+    private void RemoveVisitedRoom(SortedDictionary<DateTime, string> visitedRooms, string visitingRoom)
+    {
+        var pairToRemove = visitedRooms.FirstOrDefault(pair => string.Equals(pair.Value, visitingRoom));
+
+        if (pairToRemove.Key != default)
+        {
+            visitedRooms.Remove(pairToRemove.Key);
+        }
+    }
+
+    private void RemoveOldestVisitedRoom(SortedDictionary<DateTime, string> visitedRooms, int maxVisitedRooms)
+    {
+        if (visitedRooms.Count > maxVisitedRooms)
+        {
+            visitedRooms.Remove(visitedRooms.First().Key);
+        }
     }
 }
